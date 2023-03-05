6af6b2b0e836d584073200233e3ba4fcd493e5ae
Cameron Morgan <cameron.morgan@skiff.com>
2023-03-04 21:28:55 -0800

    Completed Task 2 tasks.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import Form from './components/Form';
 
 export default function App() {
 
-  const dummyData = [
+  const defaultTeamData = [
     { name: "one", email: "one@email.com", role: "backend engineer"},
     { name: "two", email: "two@email.com", role: "frontend engineer"},
     { name: "three", email: "three@email.com", role: "designer"},
     { name: "four", email: "four@email.com", role: "analyst"},
   ]
 
-  const [teamData, setTeamData] = useState(dummyData);
+  const defaultFormValues = { name: "", email: "default@email.com", role: "" }
+
+  const [teamData, setTeamData] = useState(defaultTeamData);
+  const [formValues, setFormValues] = useState(defaultFormValues);
+
+  const onChange = (name, value) => {
+    // spread out values and adjust the key value pair
+    setFormValues({ ...formValues, [name]: value })
+  }
+  
+  const onSubmit = () => {
+    setTeamData([formValues, ...teamData]);
+    setFormValues(defaultFormValues)
+
+  }
 
   return (
     <div className="App">
-      <form>
-        <label>Employee Name:&nbsp;
-          <input 
-            type="text"
-            name="username"
-            value=""
-          />
-        </label>
-        <label>Employee Email:&nbsp;
-          <input 
-            type="email"
-            id="email"
-            name="email"
-            value=""
-            onChange=""
-          />
-        </label>
-        <label>Employee Role:&nbsp;
-          <input 
-            type="text"
-            id="role"
-            name="role"
-            list="roles"
-            value=""
-          />
-        </label>
-        <datalist id="roles">
-            <option value="backend engineer" />
-            <option value="frontend engineer" />
-            <option value="designer" />
-            <option value="analyst" />
-          </datalist>
-
-      </form>
-      {teamData.map(teammate => {
+      <Form 
+        values={formValues}
+        change={onChange}
+        submit={onSubmit}
+      />
+      {teamData.map((teammate, idx) => {
         return (
-          <div className="list">
+          <div className="list" key={idx} >
             <div>{teammate.name}</div>
             <div>{teammate.email}</div>
             <div>{teammate.role}</div>
---
+++ src/components/Form.js
@@ -0,0 +1,65 @@
+export default function Form(props) {
+
+  const { values, change, submit } = props
+
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    change(name, value);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    submit();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="left">
+          <label>Employee Name:&nbsp;
+            <input 
+              type="text"
+              id="name"
+              name="name"
+              value={values.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>Employee Email:&nbsp;
+            <input 
+              type="email"
+              id="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>Employee Role:&nbsp;
+            <input 
+              type="text"
+              id="role"
+              name="role"
+              list="roles"
+              value={values.role}
+              onChange={handleChange}
+            />
+          </label>
+          <datalist id="roles">
+            <option value="backend engineer" />
+            <option value="frontend engineer" />
+            <option value="designer" />
+            <option value="analyst" />
+          </datalist>
+        </div>
+        <div className="right">
+          <input 
+            type="submit"
+            value="Add Member"
+          />
+        </div>
+
+      </form>
+    </>
+  )
+}
